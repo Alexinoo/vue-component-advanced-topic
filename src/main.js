47105b6ed25c7d475312bff4ea2870d01385a368
Alex Mwangi <alex@stl-horizon.com>
2022-06-05 14:03:26 +0300

    registered it as a component globally

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import App from './App.vue';
 
 // Global Component - 
 import BaseBadge from './components/BaseBadge.vue';
+import BaseCard from './components/BaseCard.vue';
 
 
 const app = createApp(App);
 
 //Registered Globally - used in multiple components
 app.component('base-badge', BaseBadge);
+app.component('base-card', BaseCard);
 
 
 app.mount('#app');
