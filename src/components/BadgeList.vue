6a8b271c5a17e10713e4350d5b9714c556a59da6
Alex Mwangi <alex@stl-horizon.com>
2022-06-05 14:04:52 +0300

    Introduction to Slots and Named Slots

--- src/components/BadgeList.vue
+++ src/components/BadgeList.vue
@@ -1,25 +1,81 @@
+
+<!-- INTRODUCTION TO SLOTS
+
+-Since we have registered BaseCard globally on the main.js
+
+- We can also control the styling of the content rendered between the <section></section> 
+
+-We can do this by wrapping the  header and the ul  content using <basecard></basecard>
+
+-Therefore we do not need the styling in the style section - That is why it is commented
+      section {
+        margin: 2rem auto;
+        max-width: 30rem;
+        border-radius: 12px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+        padding: 1rem;
+      } 
+-->
+
+<!-- NAMED  SLOTS
+
+    -Now we want to tell Vue that this content  i.e the (   <h2>Available Badges</h2> ) should go to the header slot
+  e.g.
+     <template v-slot:header>
+        <h2>Available Badges</h2>
+      </template>
+
+    -We can also be more explicit to tell Vue that the rest of the content should go to default slot
+e.g. 
+
+  <template>
+        <ul>
+          <li>
+            <base-badge type="admin" caption="ADMIN"></base-badge>
+          </li>
+          <li>
+            <base-badge type="author" caption="AUTHOR"></base-badge>
+          </li>
+        </ul>
+      </template>
+
+      -And then adding a v-slot:default;
+
+      -default is a reserved keyword and like the name applies it wlways targets the default slot
+
+
+
+ -->
+
 <template>
   <section>
-    <h2>Available Badges</h2>
-    <ul>
-      <li>
-        <base-badge type="admin" caption="ADMIN"></base-badge>
-      </li>
-      <li>
-        <base-badge type="author" caption="AUTHOR"></base-badge>
-      </li>
-    </ul>
+    <base-card>
+      <template v-slot:header>
+        <h2>Available Badges</h2>
+      </template>
+
+      <template v-slot:default>
+        <ul>
+          <li>
+            <base-badge type="admin" caption="ADMIN"></base-badge>
+          </li>
+          <li>
+            <base-badge type="author" caption="AUTHOR"></base-badge>
+          </li>
+        </ul>
+      </template>
+    </base-card>
   </section>
 </template>
 
 <style scoped>
-section {
+/* section {
   margin: 2rem auto;
   max-width: 30rem;
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
-}
+} */
 
 section h2 {
   margin: 0.5rem 0;
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,10 +1,66 @@
+<!--  INTRODUCTION TO SLOTS
+
+-Since we have registered BaseCard globally on the main.js
+
+- We can now use it to control the styling of the content rendered in the UserInfo like in the header section for example
+
+-We use it by wrapping the  content of the header and paragraph content using <basecard></basecard>
+-->
+
+
+<!-- NAMED  SLOTS
+
+    -Now we want to tell Vue that this content  i.e the ( h3 tag and the base-badge tag ) should go to the header slot
+
+    -AND FOR THAT YOU WRAP IT WITH A <template></template> tag which is a default HTML element  and on that template tag  which holds the content that should go to you named slot; 
+
+    -You add a special directive v-slot:
+
+     <header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </header>
+
+      -The v-slot directive can be used to let Vue know where certain content should go to;
+
+      -Now you let Vue  know where that content shd go to by adding an argument to this directive i.e. 
+      
+       e.g. v-slot:header
+
+      -And this tells Vue that the content inside the template will go to the named slot
+
+      e.g.
+         <template v-slot:header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+
+      -The other content i.e the paragraph will automatically go to the default slot
+
+         -We can also be more explicit to tell Vue that the rest of the content should go to default slot
+      e.g. 
+
+   <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+
+      -And then adding a v-slot:default to the template tag;
+
+      -default is a reserved keyword and like the name applies it wlways targets the default slot
+ -->
+
+
 <template>
   <section>
-    <div>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <template v-slot:header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
   </section>
 </template>
 
@@ -15,15 +71,7 @@ export default {
 </script>
 
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section div {
+section header {
   display: flex;
   justify-content: space-between;
   align-items: center;
