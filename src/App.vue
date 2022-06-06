e6c1aaa7ee8402062f638a6753bd38b0145444ca
Alex Mwangi <alex@stl-horizon.com>
2022-06-06 09:58:56 +0300

    Dynamic coponents

--- src/App.vue
+++ src/App.vue
@@ -82,40 +82,100 @@
     -->
 
 
+     <!-- DYNAMIC COMPONENTS ---
 
-    
+     -Vue has this concept of Dynamic components 
+
+     -Let's say we are building a component that has various tabs/buttons we can press to show a different component 
+
+     -i.e. In our case see Active goals / Manage goals
+
+     -Normal cases , the scenario would look like this
+
+        <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+        <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
+
+    -But this looks redundant because we have to set if conditions on each component and as your app grows, there will be a lot of repetition
+
+    -This is where dynamic content comes into play
+
+    -Vue offers a special HTML element which you can use - <component></component>
+
+    -Cannot work on its own and therefore needs a :is="" property
+
+    -The :is="" property tells component which component of all the components you define should be shown in place of this component
+
+    -We bind :is="" dynamically  to active-goals
+
+        <component :is="active-goals"></component>
+
+        -But of course we dont want to hard code this because if we di we could have just used 
+
+          <active-goals></active-goals>
+
+      -Instead what we wanna do here , is to bind :is="" and then point to the selectedComponent which holds our component HTML tags as values
+
+      -It's 'active-goals initially , but we have a method that switches between active-goals and manage-goals which are our component tags
+
+      -And that we are telling Vue that the component should swap itself for either active-goals OR manage-goals i.e. Whatever that is currently stored in the selectedComponent
+
+        <component :is="selectedComponent"></component>
+
+      -And by doing that we have less code and avoided use of multiple if-statement for each component
+
+       <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+       <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
+
+
+     -->   
 
 
 
 <template>
   <div>
     <TheHeader />
-    <BadgeList />
+    <!-- <BadgeList />
     <UserInfo :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role" />
     <course-goals #default="slotProps">
         <h2>{{slotProps.item}}</h2>
-        <p>{{slotProps['another-prop']}}</p>
+        <p>{{slotProps['another-prop']}}</p> 
     </course-goals>
+        -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+
+    <component :is="selectedComponent"></component>
+
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
 
 components : { 
     TheHeader ,
-    BadgeList,
-     UserInfo,
-    CourseGoals,
+    // BadgeList,
+    //  UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
+
 },
 
   data() {
     return {
+
+      selectedComponent : 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -123,6 +183,11 @@ components : {
       },
     };
   },
+  methods : {
+    setSelectedComponent(cmp){
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
